e530bbeec5c49310dfb713027f180afda5ec124d
gustavosilva-gss <gustavo24gss@gmail.com>
2021-10-20 16:08:53 -0300

    video and player complete

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import NewPost from "./components/new_post"
+import Player from "./components/player"
 
 export async function getStaticProps() {
     var props = {};
-    
+
     props.feelings = new Array();
 
     var res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/feelings/fetch`, {
@@ -15,10 +16,12 @@ export async function getStaticProps() {
     return { props: { props } };
 }
 
-export default function Home( { props } ) {
+export default function Home({ props }) {
     return (
-        <NewPost {...props}>
-
-        </NewPost>
+        <div>
+            <NewPost {...props}>
+            </NewPost>
+            <Player {...props}></Player>
+        </div>
     );
 };
